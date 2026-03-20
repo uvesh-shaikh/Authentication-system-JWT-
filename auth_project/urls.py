477a559a66a21441cc5ee7fdd0e4ab27d2b86f6f
uvesh-shaikh <shaikhmohduvesh@gmail.com>
2026-03-20 16:39:39 +0530

    Fix: Exclude static paths from catchall and add template dirs for static folder

--- auth_project/urls.py
+++ auth_project/urls.py
@@ -5,17 +5,25 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 from django.views.generic import TemplateView
+from django.http import HttpResponse, FileResponse
+from django.views.decorators.http import condition
+import os
 
 urlpatterns = [
     path('api/', include('auth_app.urls')),
-    # Serve HTML files
-    path('', serve, {'path': 'index.html', 'document_root': settings.STATICFILES_DIRS[0]}),
-    re_path(r'^(?P<path>.*\.html)$', serve, {'document_root': settings.STATICFILES_DIRS[0]}),
 ]
 
 # Serve static files
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # In production, use WhiteNoise via middleware, but add explicit static URL handling
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Catch all other routes and serve index.html (SPA)
+# But exclude /static/ and /api/ paths
+urlpatterns += [
+    re_path(r'^(?!static/|api/).*', TemplateView.as_view(template_name='index.html')),
+]
