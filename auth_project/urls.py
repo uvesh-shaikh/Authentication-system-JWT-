ca966c9a8193f6ebad484605fac2981ca769da55
uvesh-shaikh <shaikhmohduvesh@gmail.com>
2026-03-20 16:45:40 +0530

    Fix: Explicit static file serving with serve view and proper STATICFILES_FINDERS

--- auth_project/urls.py
+++ auth_project/urls.py
@@ -4,26 +4,20 @@ Main URL routing for auth_project.
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from django.http import HttpResponse, FileResponse
-from django.views.decorators.http import condition
-import os
+from django.views.static import serve
 
 urlpatterns = [
     path('api/', include('auth_app.urls')),
 ]
 
-# Serve static files
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    # In production, use WhiteNoise via middleware, but add explicit static URL handling
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Catch all other routes and serve index.html (SPA)
-# But exclude /static/ and /api/ paths
+# Serve static files explicitly (both DEBUG and production)
 urlpatterns += [
-    re_path(r'^(?!static/|api/).*', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
+
+# SPA catchall - serve index.html for all other routes
+urlpatterns += [
+    path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!static/).*$', TemplateView.as_view(template_name='index.html')),
 ]
